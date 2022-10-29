629800f608874b57bea06a423db8057698759872
Bhushan <bhushanjkulawade@gmail.com>
2022-10-29 22:03:03 +0530

    fibonacci code cleanup

---
+++ Week-4/4.3/fibonacci.js
@@ -0,0 +1,21 @@
+const fibonaci = (n) => ({
+  [Symbol.iterator]:()=> {
+      let i =1;
+      let number1, number2 = 0;
+      return {
+          next: () => {
+              if(i++<=n){
+                [number1, number2] = [number2, (number1+number2) || 1];
+                return { value : number1, done : false }
+              }
+              else{
+                return{ done : true }
+              }
+          }
+      }
+    
+      console.log(n);
+  }
+})
+
+console.log([...fibonaci(6)]);
